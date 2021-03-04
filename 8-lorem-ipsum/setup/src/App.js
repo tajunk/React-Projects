695ae7ee8ffb6010e58accf35d4bcad575eee9a9
Taylor <tajunk@gmail.com>
2021-03-03 18:56:18 -0700

    03/03 update

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>Tired of boring Lorem Ipsum?</h3>
+      <form onSubmit={handleSubmit} className='lorem-form'>
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'></article>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })}
+    </section>
+  );
 }
 
 export default App;
